4a0cb515e1facfd7221ac836b6f0925f15528db1
RasenRhino <ridhambhagat@gmail.com>
2023-06-30 17:02:14 +0530

    [chore]update readme and typeorm config

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,18 +1,25 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
+import { Identity } from './src/modules/identity/entities/identity.entity';
+config({
+  path: '.env',
+});
 
-config();
-
-const configService = new ConfigService();
+const configService = new ConfigService({ path: '.env' });
 
 export default new DataSource({
   type: 'mysql',
   host: configService.get<string>('TYPEORM_HOST'),
-  port: configService.get<number>('TYPEORM_PORT'),
   username: configService.get<string>('TYPEORM_USERNAME'),
   password: configService.get<string>('TYPEORM_PASSWORD'),
   database: configService.get<string>('TYPEORM_DATABASE'),
-  entities: [],
-  migrations: [],
+  port: configService.get<number>('TYPEORM_PORT'),
+  synchronize: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: [Identity],
+  migrations: ['./1688121066820-update-tables.ts'],
+  subscribers: ['dist/subscriber/**/*.js'],
 });
